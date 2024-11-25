d2e439f9cb5357268f34f213d4481c3acd9014cf
MydeG <mydeplus2023@gmail.com>
2024-11-24 21:12:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Função para detectar a plataforma e executar uma ação específica
-function getPlatform() {
-    var plataform = navigator.userAgent.toLowerCase(); // Pega o user agent e coloca em minúsculas
+// // Função para detectar a plataforma e executar uma ação específica
+// function getPlatform() {
+//     var plataform = navigator.userAgent.toLowerCase(); // Pega o user agent e coloca em minúsculas
 
-    if (plataform.indexOf("android") > -1) {
-        return "android";
-    } else if (plataform.indexOf("windows") > -1) {
-        return "windows";
-    } else {
-        return "";
-    }
-}
+//     if (plataform.indexOf("android") > -1) {
+//         return "android";
+//     } else if (plataform.indexOf("windows") > -1) {
+//         return "windows";
+//     } else {
+//         return "";
+//     }
+// }
 
-// Chama a função ao carregar a página
-var plataform = detectarPlataforma();
-if (plataform == "") {
-    alert("Plataforma desconhecida. Não foi possível determinar a ação.");
-}
+// // Chama a função ao carregar a página
+// var plataform = detectarPlataforma();
+// if (plataform == "") {
+//     alert("Plataforma desconhecida. Não foi possível determinar a ação.");
+// }
