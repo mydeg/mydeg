9be19ab4b9f4e01d402cec5e98623d92cfa21f9d
MydeG <mydeplus2023@gmail.com>
2024-11-24 21:08:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Função para detectar a plataforma e executar uma ação específica
-function detectarPlataforma() {
-    var plataforma = navigator.userAgent.toLowerCase(); // Pega o user agent e coloca em minúsculas
+function getPlatform() {
+    var plataform = navigator.userAgent.toLowerCase(); // Pega o user agent e coloca em minúsculas
 
-    if (plataforma.indexOf("android") > -1) {
+    if (plataform.indexOf("android") > -1) {
         return "android";
-    } else if (plataforma.indexOf("windows") > -1) {
+    } else if (plataform.indexOf("windows") > -1) {
         return "windows";
     } else {
         return "";
@@ -12,7 +12,7 @@ function detectarPlataforma() {
 }
 
 // Chama a função ao carregar a página
-var plataforma = detectarPlataforma();
-if (plataforma == "") {
+var plataform = detectarPlataforma();
+if (plataform == "") {
     alert("Plataforma desconhecida. Não foi possível determinar a ação.");
 }
