3c6e2a752a11567b18bdc51a7016f045fc9a1130
MydeG <mydeplus2023@gmail.com>
2024-11-24 21:57:26 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function platformCheck(urlAndroid, urlWindows){
+    var platform = navigator.userAgent.toLowerCase();
+
+    if (platform.indexOf("android") > -1) {
+        window.location.href = urlAndroid;
+    } else if (platform.indexOf("windows") > -1) {
+        window.location.href = urlWindows;
+    } else {
+        alert("Plataforma não suportada.\n\n" + platform);
+    }
+}
